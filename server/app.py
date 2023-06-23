eec18b9a116760d4829e0ea00ed77dfdfd41896c
Historia Montague <historia.montague@outlook.com>
2023-06-23 15:28:59 -0600

    added basic file structure

---
+++ server/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+CORS(app)
+migrate = Migrate(app, db)
+
+db.init_app(app)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
