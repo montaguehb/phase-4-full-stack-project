cc7072c789ac1e543ba6e5b7bed8c3092011b5cb
nolan nash <n0lan.n4sh@gmail.com>
2023-06-25 03:41:30 -0700

    create user concerts table + class, with foreign keys, no relationships or routes

--- server/models.py
+++ server/models.py
@@ -27,11 +27,24 @@ class Venue(db.Model, SerializerMixin):
     name = db.Column(db.String, nullable= False)
     capacity = db.Column(db.Integer, nullable=False)
     location = db.Column(db.VARCHAR, nullable=False)
-    
-    concerts = db.relationship('Concert',back_populates='venue')
-    
-    serialize_rules = ('-concerts',)
-    
+
     def __repr__(self):
         return f'<Venue: {self.id} \n Venue Name: {self.name}>'
 
+class User(db.Model, SerializerMixin):
+    __tablename__ = 'users'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.VARCHAR, nullable=False)
+    eamil = db.Column(db.VARCHAR, nullable=False)
+
+    def __repr__(self):
+        return f'<user: {self.id} \nname: {self.name}>'
+
+class UserConcert(db.Model, SerializerMixin):
+    __tablename__ = 'user_concerts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, ForeignKey = ('users.id'))
+    concert_id = db.Column(db.Integer, ForeignKey = ('concerts.id'))
