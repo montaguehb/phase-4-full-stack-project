02f30df20c4fd342c4752aa36c176a5c6fed7e54
nolan nash <n0lan.n4sh@gmail.com>
2023-06-25 02:20:03 -0700

    basics of table created with minimal serialization /extra features

--- server/models.py
+++ server/models.py
@@ -7,6 +7,7 @@ from flask_migrate import Migrate
 from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
 # Local imports
 
@@ -31,3 +32,17 @@ api = Api(app)
 # Instantiate CORS
 CORS(app)
 
+class Venue(db.Model, SerializerMixin):
+    __tablename__ = 'venues'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable= False)
+    capacity = db.Column(db.Integer, nullable=False)
+    location = db.Column(db.VARCHAR, nullable=False)
+    
+    concerts = db.relationship('Concert',back_populates='venue')
+    
+    serialize_rules = ('-concerts',)
+    
+    def __repr__(self):
+        return f'<Venue: {self.id} \n Venue Name: {self.name}>'
