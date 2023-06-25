a24977925026b3ac304c3c43512d519d5d33935e
nolan nash <n0lan.n4sh@gmail.com>
2023-06-25 02:25:33 -0700

    table created + prep app to add routes

--- server/app.py
+++ server/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
-from flask_cors import CORS
+from models import db, Venue
 from flask_migrate import Migrate
+from flask import Flask, request, make_response
+from flask_restful import Api, Resource
+from flask_cors import CORS
+import os
 
 from models import db
 
@@ -13,6 +16,13 @@ CORS(app)
 migrate = Migrate(app, db)
 
 db.init_app(app)
+
+api = Api(app)
+
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+class VenByID(Resource):
+# assuming that you will not need/be able to look for a list of venues
+# venue will be li
