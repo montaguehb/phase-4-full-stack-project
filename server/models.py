e2a6d4fe021b2ffea4c7d4ee7b0e918570e3669e
Historia Montague <historia.montague@outlook.com>
2023-06-26 09:19:36 -0600

    update pip file

--- server/models.py
+++ server/models.py
@@ -1,11 +1,32 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+# Standard library imports
 
-# create the extension
-db = SQLAlchemy()
-# create the app
+# Remote library imports
+from flask import Flask
+from flask_cors import CORS
+from flask_migrate import Migrate
+from flask_restful import Api
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
+
+# Local imports
+
+# Instantiate app, set attributes
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///project.db"
-# initialize the app with the extension
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+# Define metadata, instantiate db
+metadata = MetaData(naming_convention={
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+})
+db = SQLAlchemy(metadata=metadata)
+migrate = Migrate(app, db)
 db.init_app(app)
+
+# Instantiate REST API
+api = Api(app)
+
+# Instantiate CORS
+CORS(app)
+
