a0dec852e095f78437cc1a64153d8bac2f1e8881
Historia Montague <126850725+montaguehb@users.noreply.github.com>
2023-06-26 11:52:07 -0600

    finished concert route (#28)
    Co-authored-by: nolan <128182736+nolannash@users.noreply.github.com>

--- server/app.py
+++ server/app.py
@@ -1,14 +1,15 @@
-# Standard library imports
 
-# Remote library imports
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
-from flask_restful import Api
+from flask_restful import Api, Resource
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
 
+
 # Local imports
+from models import db, Concert
+
 
 # Instantiate app, set attributes
 app = Flask(__name__)
@@ -17,11 +18,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
 # Define metadata, instantiate db
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
 
-db = SQLAlchemy(metadata=metadata)
 migrate = Migrate(app, db)
 db.init_app(app)
 
@@ -31,3 +28,16 @@ api = Api(app)
 # Instantiate CORS
 CORS(app)
 
+
+class Concerts(Resource):
+    def get(self):
+        return make_response([concert.to_dict() for concert in Concert.query.all()], 200)
+
+class ConcertById(Resource):
+    def get(self, id):
+        return make_response(db.session.get(Concert, id).to_dict(), 200)
+    
+api.add_resource(Concerts, "/concerts")
+api.add_resource(ConcertById, "/concerts/<int:id>")
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,30 +1,20 @@
-# Standard library imports
-
 # Remote library imports
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-# Local imports
-
-# Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-# Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
 
-class Concert(db.Model):
-    __tablename__ = 'concerts'
 
+class Concert(db.Model, SerializerMixin):
+    __tablename__ = 'concerts'
+    
     id = db.Column(db.Integer, primary_key=True)
     date = db.Column(db.DateTime)
     venue_id = db.Column(db.Integer, db.ForeignKey("venues.id"))
     tour = db.Column(db.Integer, db.ForeignKey("tours.id"))
-
