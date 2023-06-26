cde15fcb288cfcb5eb4ca876009d57ab9af5963e
Historia Montague <historia.montague@outlook.com>
2023-06-26 11:38:46 -0600

    added concerts by id

--- server/app.py
+++ server/app.py
@@ -26,7 +26,11 @@ class Concerts(Resource):
     def get(self):
         return make_response([concert.to_dict() for concert in Concert.query.all()], 200)
 
+class ConcertById(Resource):
+    def get(self, id):
+        return make_response(db.session.get(Concert, id).to_dict(), 200)
+    
 api.add_resource(Concerts, "/concerts")
-
+api.add_resource(ConcertById, "/concerts/<int:id>")
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
