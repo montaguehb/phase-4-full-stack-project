c9eaaf94ca1f7f3101647c4224d049137e29f21e
Historia Montague <historia.montague@outlook.com>
2023-06-26 11:35:17 -0600

    cleaned up imports, added route for /concerts

--- server/app.py
+++ server/app.py
@@ -1,18 +1,32 @@
-from flask import Flask
+from flask import Flask, make_response
 from flask_cors import CORS
 from flask_migrate import Migrate
+from flask_restful import Api, Resource
 
-from models import db
+# Local imports
+from models import db, Concert
 
+# Instantiate app, set attributes
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
 
-CORS(app)
+# Define metadata, instantiate db
 migrate = Migrate(app, db)
-
 db.init_app(app)
-@app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+
+# Instantiate REST API
+api = Api(app)
+
+# Instantiate CORS
+CORS(app)
+
+class Concerts(Resource):
+    def get(self):
+        return make_response([concert.to_dict() for concert in Concert.query.all()], 200)
+
+api.add_resource(Concerts, "/concerts")
+
+if __name__ == "__main__":
+    app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,33 +1,18 @@
-# Standard library imports
-
 # Remote library imports
-from flask import Flask
-from flask_cors import CORS
-from flask_migrate import Migrate
-from flask_restful import Api
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import MetaData
+from sqlalchemy_serializer import SerializerMixin
 
-# Local imports
-
-# Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
-
-# Define metadata, instantiate db
 metadata = MetaData(naming_convention={
     "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
 })
 
 db = SQLAlchemy(metadata=metadata)
-migrate = Migrate(app, db)
-db.init_app(app)
 
-# Instantiate REST API
-api = Api(app)
-
-# Instantiate CORS
-CORS(app)
-
+class Concert(db.Model, SerializerMixin):
+    __tablename__ = 'concerts'
+    
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime)
+    venue_id = db.Column(db.Integer, db.ForeignKey("venues.id"))
+    tour = db.Column(db.Integer, db.ForeignKey("tours.id"))
