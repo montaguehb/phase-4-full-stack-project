a2fc65bd57affee1b73eac3cd96b6724ea0dbe50
Historia Montague <126850725+montaguehb@users.noreply.github.com>
2023-06-26 14:00:59 -0600

    added user auth to backend (#41)

--- server/app.py
+++ server/app.py
@@ -1,33 +1,34 @@
 
-from flask import Flask, make_response
-from flask_cors import CORS
-from flask_migrate import Migrate
+from flask import Flask, make_response, request, session
+# from flask_cors import CORS
+# from flask_migrate import Migrate
 from flask_restful import Api, Resource
 
+from config import app, db, api
 # Local imports
-from models import db, Concert, Venue, Artist
+from models import Concert, Venue, Artist, User
 
 
-# Instantiate app, set attributes
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.json.compact = False
+# # Instantiate app, set attributes
+# app = Flask(__name__)
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.json.compact = False
 
-# Define metadata, instantiate db
+# # Define metadata, instantiate db
 
-migrate = Migrate(app, db)
-db.init_app(app)
+# migrate = Migrate(app, db)
+# db.init_app(app)
 
 @app.route("/")
 def hello_world():
     return "<p>Hello, World!</p>"
 
-# Instantiate REST API
-api = Api(app)
+# # Instantiate REST API
+# api = Api(app)
 
-# Instantiate CORS
-CORS(app)
+# # Instantiate CORS
+# CORS(app)
 
 class Artists(Resource):
     def get(self):
@@ -53,12 +54,21 @@ class VenuesByID(Resource):
     def get(self,id):
         return make_response(db.session.get(Venue, id),200)
 
+class Login(Resource):
+    def post(self):
+        req = request.get_json()
+        if user := db.session.get(User, req.get("username")):
+            if user.authenticate(req.get("password")):
+                session["user_id"] = user.id
+                return make_response(user.to_dict(), 200)
+                
 api.add_resource(Concerts, "/concerts")
 api.add_resource(ConcertById, "/concerts/<int:id>")
 api.add_resource(Venues, "/venues")
 api.add_resource(VenuesByID, "/venues/<int:id>")
 api.add_resource(Artists, "/artists")
 api.add_resource(ArtistsById, "/artists/<int:id>")
+api.add_resource(Login, "/login")
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
--- server/models.py
+++ server/models.py
@@ -1,14 +1,9 @@
 # Remote library imports
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import MetaData
 from sqlalchemy_serializer import SerializerMixin
+from sqlalchemy.ext.hybrid import hybrid_property
 
-metadata = MetaData(naming_convention={
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-})
+from config import db, bcrypt
 
-db = SQLAlchemy(metadata=metadata)
 
 
 class Concert(db.Model, SerializerMixin):
@@ -43,8 +38,23 @@ class User(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     first_name = db.Column(db.String, nullable=False)
     username = db.Column(db.VARCHAR, nullable=False)
+    password_hash = db.Column(db.String)
     email = db.Column(db.VARCHAR, nullable=False)
 
+    @hybrid_property
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter()
+    def password_hash(self, password):
+        password_hash = bcrypt.generate_password_hash(
+            password.encode('utf-8'))
+        self._password_hash = password_hash.decode('utf-8')
+    
+    def authenticate(self, password):
+        return bcrypt.check_password_hash(
+            self._password_hash, password.encode('utf-8'))
+            
     def __repr__(self):
         return f'<user: {self.id} \nname: {self.name}>'
 
