099f41570d80c4cc938aa7a068551d5b82f06c18
Historia Montague <historia.montague@outlook.com>
2023-06-27 13:38:28 -0600

    added basic input validation for username and email signup

---
+++ client/src/components/Login.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
+const loginSchema = Yup.object().shape({
+  username: Yup.string()
+    .min(2, "Invalid username")
+    .max(50, "Invalid username")
+    .required("Required"),
+  password: Yup.string().required("Required"),
+});
+
+const Login = () => {
+  const handleSubmit = async (values, { setSubmitting }) => {
+    const resp = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(values),
+    });
+    if (resp.ok) {
+      const data = await resp.json();
+      console.log(data);
+    } else {
+      alert("Unable to login");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Login</h1>
+      <Formik
+        initialValues={{ name: "", email: "", username: "", password: "" }}
+        validationSchema={loginSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting }) => (
+          <Form>
+            <label>Username:</label>
+            <Field type="username" name="username" />
+            <ErrorMessage name="username" component="div" />
+            <label>Password:</label>
+            <Field type="password" name="password" />
+            <ErrorMessage name="password" component="div" />
+            <button type="submit" disabled={isSubmitting}>
+              Submit
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
+
+export default Login;
