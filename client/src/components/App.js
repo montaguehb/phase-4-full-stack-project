1d4c7eef4e61428b5753a4bcad8446e1acc5de8a
Jay Lim <102722125+Blujay0@users.noreply.github.com>
2023-06-27 10:40:17 -0600

    create and modify ConcertPage.js, ConcertCard.js, ConcertList.js (#60)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,21 +1,30 @@
-function App() {
-  return (
-    
-import Nav from "./Nav";
+import './App.css';
 import {React, useEffect, useState, useContext} from 'react';
+import Cards from "./ConcertCard";
+import Footer from "./Footer";
+import Navbar from "./Navbar";  
+import Nav from "./Nav";
 import Header from './Header.js'
 import {Route, Switch} from 'react-router-dom';
 
-import './App.css';
 
 function App() {
   return (
-
     <div className="App">
-      <Nav></Nav>
-      <Header />
-      <Switch>
-      </Switch>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
---
+++ client/src/components/ConcertList.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ConcertCard from "./ConcertCard";
+
+function ConcertList({concerts}) {
+  return (
+    <ul className="cards">{concerts.map((concert) => {
+      return <ConcertCard key={concert.id} concert={concert}/> // pass individual concert to concertcard as prop
+        }
+      )}
+    </ul>
+  );
+}
+
+export default ConcertList;
