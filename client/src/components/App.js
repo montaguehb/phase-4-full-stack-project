6aea8a0ea001aa9cbb084ce3ce4e906ab4c0a509
Jay Lim <102722125+Blujay0@users.noreply.github.com>
2023-06-28 15:51:11 -0600

    73 frontend routes jay (#77)

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,11 +5,13 @@ import {Route, Switch} from 'react-router-dom';
 import Clear from './Clear'
 import ConcertPage from './ConcertPage';
 import ConcertList from './ConcertList';
+
 import Signup from "./SignUp"
 import Login from "./Login"
 import Nav from './Nav';
 
 function App() {
+
   const [search, setSearch] = useState("")
   const [sortBy, setSortBy] = useState("name")
 
@@ -25,11 +27,24 @@ function App() {
       {/* <ConcertList search={search} sortBy={sortBy}/> */}
       <ConcertPage />
       <Footer/>
-      {/* <Switch>
-        <Route path='/'/>
-        <Signup></Signup>
-        <Login></Login>
-      </Switch> */}
+      <Switch>
+        <Route path='/'></Route>
+        <Route path='/profile'>
+          
+        </Route>
+        <Route path='/signup'>
+          <SignUp />
+        </Route>
+        <Route path='/login'>
+          <Login />
+        </Route>
+        <Route path='/concerts'>
+          <ConcertList />
+        </Route>
+        <Route path='/concerts/:id'>
+          <ConcertList />
+        </Route>
+      </Switch>
 
     </div>
   );
