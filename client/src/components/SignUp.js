c6d71fb986fe97ed7028e4a265bd3112d3d4e1bc
Jay Lim <jlimoman@gmail.com>
2023-06-29 13:42:56 -0600

    regex implemented for username and passowrd fields in SignUp form

--- client/src/components/SignUp.js
+++ client/src/components/SignUp.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { Formik, Form, Field, ErrorMessage } from "formik";
 import * as Yup from "yup";
+import re
 
 
 const SignUp = () => {
@@ -9,10 +10,19 @@ const SignUp = () => {
       .string()
       .required('Please Enter your Username')
       .min(2, "Invalid username")
-      .max(50, "Invalid username"),
+      .max(50, "Invalid username")
+      .matches(
+        // regex in react required to be b/w opening / and closing /
+        /^(?=.{4,32}$)(?![.-])(?!.*[.]{2})[a-zA-Z0-9.-]+(?<![.])$/, 
+        "Must be between 4 to 32 characters with no combination of _, -, or . in between or at the end."
+        ),
     password: Yup
       .string()
-      .required("Please Enter your Password"),
+      .required("Please Enter your Password")
+      .matches(
+        /^(?=.*[\d])(?=.*[A-Z])(?=.*[a-z])(?=.*[@#$])[\w\d@#$]{6,12}$/,
+        "Must have at least one uppercase letter, lowercase letter, symbol, and number."
+        ),
     email: Yup
       .string()
       .email()
