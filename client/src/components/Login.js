e7ba623257b1d9bc77cfb50aa2466a37792c2500
Jay Lim <jlimoman@gmail.com>
2023-06-30 12:23:03 -0600

    add error handling to Login

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -26,15 +26,22 @@ const Login = ({login, updateLogin,updateUser}) => {
   });
 
   const handleSubmit = async (values, { setSubmitting }) => {
-    const resp = await fetch("/login", {
-      method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
-      body: JSON.stringify(values),
-    });
-    updateLogin(resp.ok)
-    history.push('/')
+    try {
+      const resp = await fetch("/login", {
+        method: "POST",
+        headers: {
+          "content-type": "application/json",
+        },
+        body: JSON.stringify(values),
+      });
+      updateLogin(resp.ok)
+      history.push('/')
+      alert("Login Successful!")
+
+    } catch(e) {
+      alert("Login Failed!")
+    }
+
   };
 
   return (
